383967a100b841f2f9af440ccef8ac945fa0aa1e
kkaltenheuser <karenkaltenheuser@gmail.com>
2020-12-07 14:14:39 -0500

    index for nav

---
+++ src/components/nav/index.js
@@ -0,0 +1,16 @@
+// dependencies
+import React from "react";
+import "./style.css";
+
+// nav var
+const Nav =() => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light">
+      <div className="navbar-brand">
+        *Insert Generic Company Name*
+      </div>
+    </nav>
+  );
+}
+
+export default Nav;
